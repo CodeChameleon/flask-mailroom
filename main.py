7545cc8dcb3db3182c1fb1ff548f4e147d90e144
D Smith <dsmith82@uw.edu>
2019-04-06 00:12:05 -0700

    Add donate route and navigation links

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
-
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -14,10 +13,29 @@ def home():
 @app.route('/donations/')
 def all():
     donations = Donation.select()
-    return render_template('donations.jinja2', donations=donations)
-    
+    add_donor = request.args.get('donor')
+    return render_template('donations.jinja2', donor=add_donor, donations=donations)
+
+@app.route('/donate/', methods=["GET", "POST"])
+def donate():
+    if request.method == 'POST':
+        donor_name = request.form["donor"]
+        try:
+            amount = int(request.form["amount"])
+        except ValueError:
+            return render_template('donate.jinja2', error="Invalid Amount")
+
+        try:
+            donor = Donor.select().where(Donor.name==donor_name).get()            
+        except Donor.DoesNotExist:
+            return render_template('donate.jinja2', error="Invalid Donor")
+        else:
+            Donation(value=amount, donor=donor).save()
+            return redirect(url_for('all', donor=donor.name))
+
+    return render_template('donate.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
